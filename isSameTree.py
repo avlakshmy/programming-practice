16682f674a9809a60b1fe446ab78268d43282ed0
A V Lakshmy <37469921+avlakshmy@users.noreply.github.com>
2020-11-12 21:10:20 +0530

    Create isSameTree.py

---
+++ isSameTree.py
@@ -0,0 +1,6 @@
+def isSameTree(p, q):
+    if p == None and q == None:
+        return True
+    if p == None or q == None:
+        return False
+    return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
