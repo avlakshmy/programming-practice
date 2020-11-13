85eb0b35d7f1a124fbedfc5c0ad5194d30bf49b5
A V Lakshmy <37469921+avlakshmy@users.noreply.github.com>
2020-11-13 09:52:03 +0530

    Create deleteDuplicates.py

---
+++ deleteDuplicates.py
@@ -0,0 +1,13 @@
+def deleteDuplicates(head):
+    if head == None or head.next == None:
+        return head
+    currValue = head.val
+    print(currValue)
+    temp = head
+    while temp.next:
+        while temp.next and temp.next.val == currValue:
+            temp.next = temp.next.next
+        if temp.next:
+            currValue = temp.next.val
+            temp = temp.next
+    return head
